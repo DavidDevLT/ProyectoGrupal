46f48c363242428ee716d120c19d9f8146899297
DavidDevLT <75176074@continental.edu.pe>
2024-01-18 12:48:41 -0500

    Creando el proyecto

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,57 @@ Auth::routes();
 Route::get('/', function(){
     return redirect()->route('login');
 });
+
+Route::get('/', function () {
+    /*return view('welcome');*/
+    return "Bienvenido a la página principal";
+});
+
+//Agregando nueva ruta
+Route::get('practicas-preprofesionales', function() {
+    return "Bienvenido a la Gestión de Prácticas Preprofesionales";
+});
+
+//Agregando nueva ruta para cREAR un nuevo registro
+Route::get('practicas/create', function() {
+    return "En esta página podrás crear un nuevo proyecto.";
+});
+
+//Añadiendo rutas orientado al proyecto
+Route::get('practicas/logout', function() {
+    return "Ha salido exitosamente de su cuenta.";
+});
+
+Route::get('practicas/dashboard', function() {
+    return "Se visualiza los informes.";
+});
+
+Route::get('practicas/admin/cursos', function() {
+    return "Información de los cursos";
+});
+
+Route::get('practicas/admin/horarios', function() {
+    return "Información de los horarios";
+});
+
+Route::get('practicas/admin/mensajes', function() {
+    return "Gestión de mensajes";
+});
+
+Route::get('practicas/help', function() {
+    return "Soporte con preguntas frecuentes";
+});
+
+Route::get('practicas/contacto', function() {
+    return "Información de contactos";
+});
+
+
+//Mejorando el código con las rutas: 1 variable y 2 variables
+Route::get('practicas/{procedimiento}/{registro?}', function($procedimiento, $registro = null) {
+    if($registro) { //condicional si se indica un registro
+        return "Bienvenido al registro: $registro, del procedimiento $procedimiento";
+    } else {
+        return "Bienvenido al procedimiento: $procedimiento";
+    }
+});
